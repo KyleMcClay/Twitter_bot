6220c6dc7278fc20e1508b3cbdfae4689507d53b
Kyle Wins <37460257+KyleMcClay@users.noreply.github.com>
2019-03-20 12:24:46 -0700

    log in information

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# username XistentialTweet
+# Consumer Key (API Key)
+consumer_key  = 'x'
+# Consumer Secret (API Secret)
+consumer_secret = 'x'
+# Access Token
+access_token = 'x'
+# Access Token Secret
+access_token_secret = 'x'
